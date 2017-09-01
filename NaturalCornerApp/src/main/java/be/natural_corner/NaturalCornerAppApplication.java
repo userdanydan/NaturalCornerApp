72d8a061b0a1af83bd3495a8ccd1f9a714a4c8d2
userdanydan <userdany.dan1952@laposte.net>
2017-09-01 12:27:43 +0200

    first

---
+++ NaturalCornerApp/src/main/java/be/natural_corner/NaturalCornerAppApplication.java
@@ -0,0 +1,12 @@
+package be.natural_corner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalCornerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalCornerAppApplication.class, args);
+	}
+}
