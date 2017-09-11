70941c6a97c2a8af0ecd8da6c679ee53bd25c244
userdanydan <userdany.dan1952@laposte.net>
2017-09-11 21:11:58 +0200

    erase 4

--- MarsRoverApp/src/com/thoughtworks/mars_rover/exception/OutOfTheGridException.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.mars_rover.exception;
-
-public class OutOfTheGridException extends RuntimeException {
-	public OutOfTheGridException(String message) {
-		// TODO Auto-generated constructor stub
-		
-		super(message);
-	}
-}
